00283429ff2f29b2228ab6c7dc0ba8b707862159
Robson Cesar de Siqueira <sistemas.robson@gmail.com>
2024-12-30 10:29:18 -0300

    Initial Commit

---
+++ AdvancedUIKitSample/AdvancedUIKitSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvancedUIKitSample
+//
+//  Created by Robson Cesar de Siqueira on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AdvancedUIKitSample/AdvancedUIKitSampleTests/AdvancedUIKitSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedUIKitSampleTests.swift
+//  AdvancedUIKitSampleTests
+//
+//  Created by Robson Cesar de Siqueira on 30/12/24.
+//
+
+import Testing
+@testable import AdvancedUIKitSample
+
+struct AdvancedUIKitSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
